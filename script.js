5f407475ff6e88c3989e0109029592ded4284e5d
Luka929-del <luka.natsvlishvili.1@btu.edu.ge>
2025-05-22 18:58:30 +0400

    write some code in js
    I wrote some code in JS file to solve the exercise was given by my lecturer

---
+++ script.js
@@ -0,0 +1,20 @@
+let temperature = prompt("შეიყვანეთ ტემპერატურა: ");
+switch(true) {
+    case temperature < 0:
+        console.log("ყინავს");
+        break;
+    case (temperature >= 0 && temperature <= 10):
+        console.log("ძალიან ცივა");
+        break;
+    case (temperature >= 11 && temperature <= 20):
+        console.log("ცივა");
+        break;
+    case (temperature >= 21 && temperature <= 30):
+        console.log("თბილა");
+        break;
+    case temperature > 30:
+        console.log("ცხელა");
+        break;
+    default:
+        console.log("არასწორი მონაცემია შეყვანილი")
+}
